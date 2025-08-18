452e4a16324c3a5709f810ef9ae1d5acb16fce8e
Bhairavi Gawas <bhairavigawas9@gmail.com>
2025-08-18 18:27:41 +0530

    feat: built in pipes in nestjs

--- intro/src/user/user.controller.ts
+++ intro/src/user/user.controller.ts
@@ -1,21 +1,60 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { UsersService } from './users.service';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  ParseIntPipe,
+  DefaultValuePipe
+} from "@nestjs/common";
+import { UsersService } from "./users.service";
 
 // GET -- http://localhost:3000/users
-@Controller('users')
+@Controller("users")
 export class UsersController {
   @Get()
-  getUsers() {
+  getUsers(
+    @Query("limit", new DefaultValuePipe(10), ParseIntPipe ) limit: number,
+    @Query() query: any,
+    @Query("page", new DefaultValuePipe(1), ParseIntPipe) page: number
+  ) {
     const displayUsers = new UsersService();
-    return { message: 'Users fetched successfully', users: displayUsers.getAllUsers()}
+    if (query.gender) {
+      return {
+        message: "Users fetched successfully according to query params",
+        users: displayUsers
+          .getAllUsers()
+          .filter((user) => user.gender === query.gender),
+      };
+    }
+    // console.log(query);
+    console.log(limit, page);
+    return {
+      message: "Users fetched successfully",
+      users: displayUsers.getAllUsers(),
+    };
   }
 
   //POST -- http://localhost:3000/users
   @Post()
   createUser() {
-    const user = {id: 5, name: 'Johny', age: 30, gender: 'Male', isMarried: false };
+    const user = {
+      id: 5,
+      name: "Johny",
+      email: "johny@gmail.com",
+      gender: "Male",
+      isMarried: false,
+    };
     const newUser = new UsersService();
     newUser.createUser(user);
-    return {message: 'User created', user};
+    return { message: "User created", user };
+  }
+
+  @Get(":id")
+  getUserByID(@Param("id", ParseIntPipe) id: number) {
+    const user = new UsersService();
+    // user.getUserById(+id);
+    console.log(typeof id, id);
+    return { message: "User fetched successfully", user: user.getUserById(id) };
   }
 }
--- intro/src/user/users.service.ts
+++ intro/src/user/users.service.ts
@@ -1,9 +1,9 @@
 export class UsersService{
-    users: {id: number; name: string; age: number, gender: string, isMarried: boolean } [] = [
-        {id : 1, name: 'John Doe', age: 30, gender: 'Male', isMarried: false},
-        {id : 2, name: 'Jane Doe', age: 24, gender: 'Female', isMarried: true },
-        {id : 3, name: 'Alex', age: 28, gender: 'Male', isMarried: false },
-        {id : 4, name: 'Emily', age: 35, gender: 'Female', isMarried: true }
+    users: {id: number; name: string; email: string, gender: string, isMarried: boolean } [] = [
+        {id : 1, name: 'John', email: 'john@gmail.com', gender: 'Male', isMarried: false},
+        {id : 2, name: 'Jane Doe', email: 'jane@gmail.com', gender: 'Female', isMarried: true },
+        {id : 3, name: 'Alex', email: 'alex@gmail.com', gender: 'Male', isMarried: false },
+        {id : 4, name: 'Emily', email: 'emily@gmail.com', gender: 'Female', isMarried: true }
     ]
     getAllUsers(){
         return this.users;
@@ -13,7 +13,7 @@ export class UsersService{
         return this.users.find(user => user.id === id);
     }
 
-    createUser(user: {id: number; name: string; age: number; gender: string; isMarried: boolean }){
+    createUser(user: {id: number; name: string; email: string;  gender: string; isMarried: boolean }){
         this.users.push(user);
     }
 }
